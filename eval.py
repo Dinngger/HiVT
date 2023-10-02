script with wrong result.

eval.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from argparse import ArgumentParser
-
+import torch
 import pytorch_lightning as pl
 from mypyg.data import DataLoader
 
@@ -34,7 +34,11 @@ if __name__ == '__main__':
 
     trainer = pl.Trainer()
     model = HiVT.load_from_checkpoint(checkpoint_path=args.ckpt_path, parallel=True)
+
     val_dataset = ArgoverseV1Dataset(root=args.root, split='val', local_radius=model.hparams.local_radius)
     dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers,
                             pin_memory=args.pin_memory, persistent_workers=args.persistent_workers)
     trainer.validate(model, dataloader)
+
+    script = model.to_torchscript()
+    torch.jit.save(script, "model.pt")

models/global_interactor.py
@@ -54,7 +54,7 @@ class GlobalInteractor(nn.Module):
     def forward(self,
                 data: Data,
                 local_embed: torch.Tensor) -> torch.Tensor:
-        edge_index, _ = subgraph(subset=~data['padding_mask'][:, self.historical_steps - 1], edge_index=data.edge_index)
+        edge_index, _ = subgraph(subset=~data['padding_mask'][:, self.historical_steps - 1], edge_index=data['edge_index'])
         rel_pos = data['positions'][edge_index[0], self.historical_steps - 1] - data['positions'][
             edge_index[1], self.historical_steps - 1]
         if data['rotate_mat'] is None:
@@ -138,14 +138,14 @@ class GlobalInteractorLayer(MessagePassing):
         return inputs + gate * (self.lin_self(x) - inputs)
 
     def propagate(self, edge_index: torch.Tensor, x, edge_attr):
-        size: List[Optional[int]] = [None, None]
+        size: List[int] = [-1, -1]
 
         i, j = 1, 0
         x_i = self._collect(x, edge_index, size, i)
         x_j = self._collect(x, edge_index, size, j)
         
         index = edge_index[i]
-        dim_size = size_i = size[i] if size[i] is not None else size[j]
+        dim_size = size_i = size[i] if size[i] >= 0 else size[j]
 
         out = self.message(x_i, x_j, edge_attr, index, size_i)
         out = self.aggregate(out, index, dim_size)

models/hivt.py
@@ -89,15 +89,18 @@ class HiVT(pl.LightningModule):
 
     def forward(self, data: Data):
         if self.rotate:
-            rotate_mat = torch.empty(data.num_nodes, 2, 2, device=self.device)
-            sin_vals = torch.sin(data['rotate_angles'])
-            cos_vals = torch.cos(data['rotate_angles'])
+            rotate_angles = data['rotate_angles']
+            num_nodes = data.num_nodes
+            assert num_nodes is not None
+            rotate_mat = torch.empty(num_nodes, 2, 2, device=rotate_angles.device)
+            sin_vals = torch.sin(rotate_angles)
+            cos_vals = torch.cos(rotate_angles)
             rotate_mat[:, 0, 0] = cos_vals
             rotate_mat[:, 0, 1] = -sin_vals
             rotate_mat[:, 1, 0] = sin_vals
             rotate_mat[:, 1, 1] = cos_vals
-            if data.y is not None:
-                data.y = torch.bmm(data.y, rotate_mat)
+            if 'y' in data.keys:
+                data['y'] = torch.bmm(data['y'], rotate_mat)
             data['rotate_mat'] = rotate_mat
         else:
             data['rotate_mat'] = None
@@ -112,10 +115,10 @@ class HiVT(pl.LightningModule):
         reg_mask = ~data['padding_mask'][:, self.historical_steps:]
         valid_steps = reg_mask.sum(dim=-1)
         cls_mask = valid_steps > 0
-        l2_norm = (torch.norm(y_hat[:, :, :, : 2] - data.y, p=2, dim=-1) * reg_mask).sum(dim=-1)  # [F, N]
+        l2_norm = (torch.norm(y_hat[:, :, :, : 2] - data['y'], p=2, dim=-1) * reg_mask).sum(dim=-1)  # [F, N]
         best_mode = l2_norm.argmin(dim=0)
         y_hat_best = y_hat[best_mode, torch.arange(data.num_nodes)]
-        reg_loss = self.reg_loss(y_hat_best[reg_mask], data.y[reg_mask])
+        reg_loss = self.reg_loss(y_hat_best[reg_mask], data['y'][reg_mask])
         soft_target = F.softmax(-l2_norm[:, cls_mask] / valid_steps[cls_mask], dim=0).t().detach()
         cls_loss = self.cls_loss(pi[cls_mask], soft_target)
         loss = reg_loss + cls_loss
@@ -125,17 +128,17 @@ class HiVT(pl.LightningModule):
     def validation_step(self, data, batch_idx):
         y_hat, pi = self(data)
         reg_mask = ~data['padding_mask'][:, self.historical_steps:]
-        l2_norm = (torch.norm(y_hat[:, :, :, : 2] - data.y, p=2, dim=-1) * reg_mask).sum(dim=-1)  # [F, N]
+        l2_norm = (torch.norm(y_hat[:, :, :, : 2] - data['y'], p=2, dim=-1) * reg_mask).sum(dim=-1)  # [F, N]
         best_mode = l2_norm.argmin(dim=0)
         y_hat_best = y_hat[best_mode, torch.arange(data.num_nodes)]
-        reg_loss = self.reg_loss(y_hat_best[reg_mask], data.y[reg_mask])
+        reg_loss = self.reg_loss(y_hat_best[reg_mask], data['y'][reg_mask])
         self.log('val_reg_loss', reg_loss, prog_bar=True, on_step=False, on_epoch=True, batch_size=1)
 
         y_hat_agent = y_hat[:, data['agent_index'], :, : 2]
-        y_agent = data.y[data['agent_index']]
+        y_agent = data['y'][data['agent_index']]
         fde_agent = torch.norm(y_hat_agent[:, :, -1] - y_agent[:, -1], p=2, dim=-1)
         best_mode_agent = fde_agent.argmin(dim=0)
-        y_hat_best_agent = y_hat_agent[best_mode_agent, torch.arange(data.num_graphs)]
+        y_hat_best_agent = y_hat_agent[best_mode_agent, torch.arange(data.__num_graphs__)]
         self.minADE.update(y_hat_best_agent, y_agent)
         self.minFDE.update(y_hat_best_agent, y_agent)
         self.minMR.update(y_hat_best_agent, y_agent)

models/local_encoder.py
@@ -22,8 +22,7 @@ from mypyg.utils import softmax, subgraph
 
 from models import MultipleInputEmbedding
 from models import SingleInputEmbedding
-from utils import DistanceDropEdge
-from utils import init_weights
+from utils import distance_drop_edge, init_weights
 
 
 class LocalEncoder(nn.Module):
@@ -41,8 +40,7 @@ class LocalEncoder(nn.Module):
         super(LocalEncoder, self).__init__()
         self.historical_steps = historical_steps
         self.parallel = parallel
-
-        self.drop_edge = DistanceDropEdge(local_radius)
+        self.local_radius = float(local_radius)
         self.aa_encoder = AAEncoder(historical_steps=historical_steps,
                                     node_dim=node_dim,
                                     edge_dim=edge_dim,
@@ -67,24 +65,23 @@ class LocalEncoder(nn.Module):
             data[f'edge_attr_{t}'] = \
                 data['positions'][data[f'edge_index_{t}'][0], t] - data['positions'][data[f'edge_index_{t}'][1], t]
         # if self.parallel:
-        snapshots = [None] * self.historical_steps
+        snapshots: List[Data] = []
         for t in range(self.historical_steps):
-            edge_index, edge_attr = self.drop_edge(data[f'edge_index_{t}'], data[f'edge_attr_{t}'])
-            snapshots[t] = Data(x=data.x[:, t], edge_index=edge_index, edge_attr=edge_attr,
-                                num_nodes=data.num_nodes)
+            edge_index, edge_attr = distance_drop_edge(self.local_radius, data[f'edge_index_{t}'], data[f'edge_attr_{t}'])
+            snapshots.append(Data(num_nodes=data.num_nodes, kwargs={'x': data['x'][:, t], 'edge_index': edge_index, 'edge_attr': edge_attr}))
         batch = combine_batch_data(snapshots)
-        out = self.aa_encoder(x=batch.x, t=None, edge_index=batch.edge_index, edge_attr=batch.edge_attr,
+        out = self.aa_encoder(x=batch['x'], t=None, edge_index=batch['edge_index'], edge_attr=batch['edge_attr'],
                                 bos_mask=data['bos_mask'], rotate_mat=data['rotate_mat'])
         out = out.view(self.historical_steps, out.shape[0] // self.historical_steps, -1)
         # else:
         #     out = [None] * self.historical_steps
         #     for t in range(self.historical_steps):
-        #         edge_index, edge_attr = self.drop_edge(data[f'edge_index_{t}'], data[f'edge_attr_{t}'])
-        #         out[t] = self.aa_encoder(x=data.x[:, t], t=t, edge_index=edge_index, edge_attr=edge_attr,
+        #         edge_index, edge_attr = distance_drop_edge(self.local_radius, data[f'edge_index_{t}'], data[f'edge_attr_{t}'])
+        #         out[t] = self.aa_encoder(x=data['x'][:, t], t=t, edge_index=edge_index, edge_attr=edge_attr,
         #                                  bos_mask=data['bos_mask'][:, t], rotate_mat=data['rotate_mat'])
         #     out = torch.stack(out)  # [T, N, D]
         out = self.temporal_encoder(x=out, padding_mask=data['padding_mask'][:, : self.historical_steps])
-        edge_index, edge_attr = self.drop_edge(data['lane_actor_index'], data['lane_actor_vectors'])
+        edge_index, edge_attr = distance_drop_edge(self.local_radius, data['lane_actor_index'], data['lane_actor_vectors'])
         out = self.al_encoder(x=(data['lane_vectors'], out), edge_index=edge_index, edge_attr=edge_attr,
                               is_intersections=data['is_intersections'], turn_directions=data['turn_directions'],
                               traffic_controls=data['traffic_controls'], rotate_mat=data['rotate_mat'])
@@ -192,14 +189,14 @@ class AAEncoder(MessagePassing):
         return inputs + gate * (self.lin_self(center_embed) - inputs)
 
     def propagate(self, edge_index: torch.Tensor, x, center_embed, edge_attr, rotate_mat: Optional[torch.Tensor]):
-        size: List[Optional[int]] = [None, None]
+        size: List[int] = [-1, -1]
 
         i, j = 1, 0
         center_embed_i = self._collect(center_embed, edge_index, size, i)
         x_j = self._collect(x, edge_index, size, j)
         
         index = edge_index[i]
-        dim_size = size_i = size[i] if size[i] is not None else size[j]
+        dim_size = size_i = size[i] if size[i] >= 0 else size[j]
 
         out = self.message(edge_index, center_embed_i, x_j, edge_attr, rotate_mat, index, size_i)
         out = self.aggregate(out, index, dim_size)
@@ -396,7 +393,7 @@ class ALEncoder(MessagePassing):
 
     def propagate(self, edge_index: torch.Tensor, x: Tuple[torch.Tensor, torch.Tensor], edge_attr,
                   is_intersections, turn_directions, traffic_controls, rotate_mat: Optional[torch.Tensor]):
-        size: List[Optional[int]] = [None, None]
+        size: List[int] = [-1, -1]
 
         i, j = 1, 0
         x_i = self._collect_tuple(x, edge_index, size, i)
@@ -406,7 +403,7 @@ class ALEncoder(MessagePassing):
         traffic_controls_j = self._collect(traffic_controls, edge_index, size, j)
         
         index = edge_index[i]
-        dim_size = size_i = size[i] if size[i] is not None else size[j]
+        dim_size = size_i = size[i] if size[i] >= 0 else size[j]
 
         out = self.message(edge_index, x_i, x_j, edge_attr, is_intersections_j, turn_directions_j, traffic_controls_j, rotate_mat, index, size_i)
         out = self.aggregate(out, index, dim_size)

mypyg/conv.py
@@ -117,9 +117,9 @@ class MessagePassing(torch.nn.Module):
         r"""Runs the forward pass of the module."""
         pass
 
-    def _set_size(self, size: List[Optional[int]], dim: int, src: Tensor):
+    def _set_size(self, size: List[int], dim: int, src: Tensor):
         the_size = size[dim]
-        if the_size is None:
+        if the_size < 0:
             size[dim] = src.size(self.node_dim)
         elif the_size != src.size(self.node_dim):
             raise ValueError(
@@ -130,14 +130,14 @@ class MessagePassing(torch.nn.Module):
         index = edge_index[dim]
         return src.index_select(self.node_dim, index)
 
-    def _collect_tuple(self, data: Tuple[Tensor, Tensor], edge_index, size: List[Optional[int]], dim: int):
+    def _collect_tuple(self, data: Tuple[Tensor, Tensor], edge_index, size: List[int], dim: int):
         assert len(data) == 2
         if isinstance(data[1 - dim], Tensor):
             self._set_size(size, 1 - dim, data[1 - dim])
         data = data[dim]
         return self._collect(data, edge_index, size, dim)
 
-    def _collect(self, data: Tensor, edge_index, size: List[Optional[int]], dim: int):
+    def _collect(self, data: Tensor, edge_index, size: List[int], dim: int):
         if isinstance(data, Tensor):
             self._set_size(size, dim, data)
             data = self._lift(data, edge_index, dim)

mypyg/data.py
@@ -7,8 +7,10 @@ import warnings
 import inspect
 from typing import (
     Any,
+    Literal,
     Callable,
     List,
+    Dict,
     Optional,
     Tuple,
     Union,
@@ -21,7 +23,7 @@ import torch
 from torch import Tensor
 
 
-class Data(object):
+class Data:
     r"""A plain old python object modeling a single graph with various
     (optional) attributes:
 
@@ -43,78 +45,31 @@ class Data(object):
 
     The data object is not restricted to these attributes and can be extented
     by any other additional data.
-
-    Example::
-
-        data = Data(x=x, edge_index=edge_index)
-        data.train_idx = torch.tensor([...], dtype=torch.long)
-        data.test_mask = torch.tensor([...], dtype=torch.bool)
     """
-    def __init__(self, x=None, edge_index=None, edge_attr=None, y=None,
-                 pos=None, normal=None, face=None, **kwargs):
-        self.x = x
-        self.edge_index = edge_index
-        self.edge_attr = edge_attr
-        self.y = y
-        self.pos = pos
-        self.normal = normal
-        self.face = face
-        for key, item in kwargs.items():
-            if key == 'num_nodes':
-                self.__num_nodes__ = item
-            else:
-                self[key] = item
+    def __init__(self, num_nodes: Optional[int], kwargs: Dict[str, Tensor]):
+        self.datas: Dict[str, Tensor] = kwargs
+        self.__num_nodes__: Optional[int] = num_nodes
+        self.__num_graphs__: int = 1
 
-        if edge_index is not None and edge_index.dtype != torch.long:
-            raise ValueError(
-                (f'Argument `edge_index` needs to be of type `torch.long` but '
-                 f'found type `{edge_index.dtype}`.'))
-
-        if face is not None and face.dtype != torch.long:
-            raise ValueError(
-                (f'Argument `face` needs to be of type `torch.long` but found '
-                 f'type `{face.dtype}`.'))
-
-    def __getitem__(self, key):
+    def __getitem__(self, key: str):
         r"""Gets the data of the attribute :obj:`key`."""
-        return getattr(self, key, None)
+        return self.datas[key]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: str, value: Optional[Tensor]):
         """Sets the attribute :obj:`key` to :obj:`value`."""
-        setattr(self, key, value)
+        if value is not None:
+            self.datas[key] = value
 
     @property
     def keys(self):
         r"""Returns all names of graph attributes."""
-        keys = [key for key in self.__dict__.keys() if self[key] is not None]
-        keys = [key for key in keys if key[:2] != '__' and key[-2:] != '__']
-        return keys
+        return self.datas.keys()
 
     def __len__(self):
         r"""Returns the number of all present attributes."""
         return len(self.keys)
 
-    def __contains__(self, key):
-        r"""Returns :obj:`True`, if the attribute :obj:`key` is present in the
-        data."""
-        return key in self.keys
-
-    def __iter__(self):
-        r"""Iterates over all present attributes in the data, yielding their
-        attribute names and content."""
-        for key in sorted(self.keys):
-            yield key, self[key]
-
-    def __call__(self, *keys):
-        r"""Iterates over all attributes :obj:`*keys` in the data, yielding
-        their attribute names and content.
-        If :obj:`*keys` is not given this method will iterative over all
-        present attributes."""
-        for key in sorted(self.keys) if not keys else keys:
-            if key in self:
-                yield key, self[key]
-
-    def __cat_dim__(self, key, value):
+    def __cat_dim__(self, key: str, value):
         r"""Returns the dimension for which :obj:`value` of attribute
         :obj:`key` will get concatenated when creating batches.
 
@@ -126,11 +81,11 @@ class Data(object):
         """
         # By default, concatenate sparse matrices diagonally.
         # Concatenate `*index*` and `*face*` attributes in the last dimension.
-        if bool(re.search('(index|face)', key)):
+        if key.find('index') != -1:
             return -1
         return 0
 
-    def __inc__(self, key, value):
+    def __inc__(self, key: str, value) -> int:
         r"""Returns the incremental count to cumulatively increase the value
         of the next attribute of :obj:`key` when creating batches.
 
@@ -142,7 +97,14 @@ class Data(object):
         """
         # Only `*index*` and `*face*` attributes should be cumulatively summed
         # up when creating batches.
-        return self.num_nodes if bool(re.search('(index|face)', key)) else 0
+        num_nodes = self.num_nodes
+        assert num_nodes is not None
+        if key == 'lane_actor_index':
+            return torch.tensor([[self['lane_vectors'].size(0)], [num_nodes]])
+        if key.find('index') != -1:
+            return num_nodes
+        else:
+            return 0
 
     @property
     def num_nodes(self):
@@ -161,95 +123,57 @@ class Data(object):
             explicitly via :obj:`data.num_nodes = ...`.
             You will be given a warning that requests you to do so.
         """
-        if hasattr(self, '__num_nodes__'):
+        if self.__num_nodes__ is not None:
             return self.__num_nodes__
-        for key, item in self('x', 'pos', 'normal', 'batch'):
-            return item.size(self.__cat_dim__(key, item))
+        for key in ['x', 'pos', 'normal']:
+            if key in self.keys:
+                item = self[key]
+                return item.size(self.__cat_dim__(key, item))
         return None
 
-    def __apply__(self, item, func):
-        if torch.is_tensor(item):
-            return func(item)
-        elif isinstance(item, (tuple, list)):
-            return [self.__apply__(v, func) for v in item]
-        elif isinstance(item, dict):
-            return {k: self.__apply__(v, func) for k, v in item.items()}
-        else:
-            return item
-
-    def apply(self, func, *keys):
-        r"""Applies the function :obj:`func` to all tensor attributes
-        :obj:`*keys`. If :obj:`*keys` is not given, :obj:`func` is applied to
-        all present attributes.
-        """
-        for key, item in self(*keys):
-            self[key] = self.__apply__(item, func)
+    def contiguous(self):
+        for key in sorted(self.keys):
+            if key in self.keys:
+                self[key] = self[key].contiguous()
         return self
 
-    def contiguous(self, *keys):
-        r"""Ensures a contiguous memory layout for all attributes :obj:`*keys`.
-        If :obj:`*keys` is not given, all present attributes are ensured to
-        have a contiguous memory layout."""
-        return self.apply(lambda x: x.contiguous(), *keys)
+    def to(self, device: torch.device):
+        for key in sorted(self.keys):
+            if key in self.keys:
+                self[key] = self[key].to(device=device)
+        return self
 
-    def to(self, device, *keys, **kwargs):
-        r"""Performs tensor dtype and/or device conversion to all attributes
-        :obj:`*keys`.
-        If :obj:`*keys` is not given, the conversion is applied to all present
-        attributes."""
-        return self.apply(lambda x: x.to(device, **kwargs), *keys)
+    def cpu(self):
+        for key in sorted(self.keys):
+            if key in self.keys:
+                self[key] = self[key].cpu()
+        return self
 
-    def cpu(self, *keys):
-        r"""Copies all attributes :obj:`*keys` to CPU memory.
-        If :obj:`*keys` is not given, the conversion is applied to all present
-        attributes."""
-        return self.apply(lambda x: x.cpu(), *keys)
-
-    def cuda(self, device=None, non_blocking=False, *keys):
-        r"""Copies all attributes :obj:`*keys` to CUDA memory.
-        If :obj:`*keys` is not given, the conversion is applied to all present
-        attributes."""
-        return self.apply(
-            lambda x: x.cuda(device=device, non_blocking=non_blocking), *keys)
-
-    def clone(self):
-        r"""Performs a deep-copy of the data object."""
-        return self.__class__.from_dict({
-            k: v.clone() if torch.is_tensor(v) else copy.deepcopy(v)
-            for k, v in self.__dict__.items()
-        })
-
-    def pin_memory(self, *keys):
-        r"""Copies all attributes :obj:`*keys` to pinned memory.
-        If :obj:`*keys` is not given, the conversion is applied to all present
-        attributes."""
-        return self.apply(lambda x: x.pin_memory(), *keys)
+    def pin_memory(self):
+        for key in sorted(self.keys):
+            if key in self.keys:
+                self[key] = self[key].pin_memory()
+        return self
 
 
 IndexType = Union[slice, Tensor, np.ndarray, Sequence]
 
 
-def combine_batch_data(data_list):
-    keys = list(set(data_list[0].keys))
-    assert 'batch' not in keys and 'ptr' not in keys
+def combine_batch_data(data_list: List[Data]):
+    keys: List[str] = list(data_list[0].keys)
 
-    batch = Data()
-    for key in data_list[0].__dict__.keys():
-        if key[:2] != '__' and key[-2:] != '__':
-            batch[key] = None
-
+    batch = Data(None, {})
     batch.__num_graphs__ = len(data_list)
-    batch.__data_class__ = data_list[0].__class__
-    for key in keys + ['batch']:
-        batch[key] = []
-    batch['ptr'] = [0]
 
-    device = None
+    temp_dict: Dict[str, List[Tensor]] = {}
+    for key in keys:
+        temp_dict[key] = []
+
     slices = {key: [0] for key in keys}
     cumsum = {key: [0] for key in keys}
-    cat_dims = {}
-    num_nodes_list = []
-    for i, data in enumerate(data_list):
+    cat_dims: Dict[str, int] = {}
+    num_nodes_list: List[int] = []
+    for data in data_list:
         for key in keys:
             item = data[key]
 
@@ -265,161 +189,35 @@ def combine_batch_data(data_list):
             cat_dims[key] = cat_dim
 
             # Add a batch dimension to items whose `cat_dim` is `None`:
-            if isinstance(item, Tensor):
+            if item.size():
                 size = item.size(cat_dim)
-                device = item.device
 
-            batch[key].append(item)  # Append item to the attribute list.
+            temp_dict[key].append(item)  # Append item to the attribute list.
 
             slices[key].append(size + slices[key][-1])
             inc = data.__inc__(key, item)
             cumsum[key].append(inc + cumsum[key][-1])
 
-        if hasattr(data, '__num_nodes__'):
-            num_nodes_list.append(data.__num_nodes__)
+        data_num_nodes = data.__num_nodes__
+        if data_num_nodes is not None:
+            num_nodes_list.append(data_num_nodes)
 
-        num_nodes = data.num_nodes
-        if num_nodes is not None:
-            item = torch.full((num_nodes, ), i, dtype=torch.long,
-                                device=device)
-            batch.batch.append(item)
-            batch.ptr.append(batch.ptr[-1] + num_nodes)
-
-    batch.batch = None if len(batch.batch) == 0 else batch.batch
-    batch.ptr = None if len(batch.ptr) == 1 else batch.ptr
-    batch.__slices__ = slices
-    batch.__cumsum__ = cumsum
-    batch.__cat_dims__ = cat_dims
-    batch.__num_nodes_list__ = num_nodes_list
+    # batch.__slices__ = slices
+    # batch.__cumsum__ = cumsum
+    # batch.__cat_dims__ = cat_dims
+    # batch.__num_nodes_list__ = num_nodes_list
 
     ref_data = data_list[0]
-    for key in batch.keys:
-        items = batch[key]
+    for key in keys:
+        items = temp_dict[key]
         item = items[0]
         cat_dim = ref_data.__cat_dim__(key, item)
-        cat_dim = 0 if cat_dim is None else cat_dim
-        if isinstance(item, Tensor):
+        if item.size():
             batch[key] = torch.cat(items, cat_dim)
-        elif isinstance(item, (int, float)):
+        else:
             batch[key] = torch.tensor(items)
 
     return batch.contiguous()
-
-
-class Batch(Data):
-    @classmethod
-    def from_data_list(cls, data_list, follow_batch=[], exclude_keys=[]):
-        keys = list(set(data_list[0].keys) - set(exclude_keys))
-        assert 'batch' not in keys and 'ptr' not in keys
-
-        batch = cls()
-        for key in data_list[0].__dict__.keys():
-            if key[:2] != '__' and key[-2:] != '__':
-                batch[key] = None
-
-        batch.__num_graphs__ = len(data_list)
-        batch.__data_class__ = data_list[0].__class__
-        for key in keys + ['batch']:
-            batch[key] = []
-        batch['ptr'] = [0]
-
-        device = None
-        slices = {key: [0] for key in keys}
-        cumsum = {key: [0] for key in keys}
-        cat_dims = {}
-        num_nodes_list = []
-        for i, data in enumerate(data_list):
-            for key in keys:
-                item = data[key]
-
-                # Increase values by `cumsum` value.
-                cum = cumsum[key][-1]
-                if isinstance(item, Tensor) and item.dtype != torch.bool:
-                    if not isinstance(cum, int) or cum != 0:
-                        item = item + cum
-                elif isinstance(item, (int, float)):
-                    item = item + cum
-
-                # Gather the size of the `cat` dimension.
-                size = 1
-                cat_dim = data.__cat_dim__(key, data[key])
-                # 0-dimensional tensors have no dimension along which to
-                # concatenate, so we set `cat_dim` to `None`.
-                if isinstance(item, Tensor) and item.dim() == 0:
-                    cat_dim = None
-                cat_dims[key] = cat_dim
-
-                # Add a batch dimension to items whose `cat_dim` is `None`:
-                if isinstance(item, Tensor) and cat_dim is None:
-                    cat_dim = 0  # Concatenate along this new batch dimension.
-                    item = item.unsqueeze(0)
-                    device = item.device
-                elif isinstance(item, Tensor):
-                    size = item.size(cat_dim)
-                    device = item.device
-
-                batch[key].append(item)  # Append item to the attribute list.
-
-                slices[key].append(size + slices[key][-1])
-                inc = data.__inc__(key, item)
-                if isinstance(inc, (tuple, list)):
-                    inc = torch.tensor(inc)
-                cumsum[key].append(inc + cumsum[key][-1])
-
-                if key in follow_batch:
-                    if isinstance(size, Tensor):
-                        for j, size in enumerate(size.tolist()):
-                            tmp = f'{key}_{j}_batch'
-                            batch[tmp] = [] if i == 0 else batch[tmp]
-                            batch[tmp].append(
-                                torch.full((size, ), i, dtype=torch.long,
-                                           device=device))
-                    else:
-                        tmp = f'{key}_batch'
-                        batch[tmp] = [] if i == 0 else batch[tmp]
-                        batch[tmp].append(
-                            torch.full((size, ), i, dtype=torch.long,
-                                       device=device))
-
-            if hasattr(data, '__num_nodes__'):
-                num_nodes_list.append(data.__num_nodes__)
-            else:
-                num_nodes_list.append(None)
-
-            num_nodes = data.num_nodes
-            if num_nodes is not None:
-                item = torch.full((num_nodes, ), i, dtype=torch.long,
-                                  device=device)
-                batch.batch.append(item)
-                batch.ptr.append(batch.ptr[-1] + num_nodes)
-
-        batch.batch = None if len(batch.batch) == 0 else batch.batch
-        batch.ptr = None if len(batch.ptr) == 1 else batch.ptr
-        batch.__slices__ = slices
-        batch.__cumsum__ = cumsum
-        batch.__cat_dims__ = cat_dims
-        batch.__num_nodes_list__ = num_nodes_list
-
-        ref_data = data_list[0]
-        for key in batch.keys:
-            items = batch[key]
-            item = items[0]
-            cat_dim = ref_data.__cat_dim__(key, item)
-            cat_dim = 0 if cat_dim is None else cat_dim
-            if isinstance(item, Tensor):
-                batch[key] = torch.cat(items, cat_dim)
-            elif isinstance(item, (int, float)):
-                batch[key] = torch.tensor(items)
-
-        return batch.contiguous()
-
-    def __getitem__(self, idx: str):
-        return super(Batch, self).__getitem__(idx)
-
-    @property
-    def num_graphs(self) -> int:
-        """Returns the number of graphs in the batch."""
-        return self.__num_graphs__
 
 
 def files_exist(files: List[str]) -> bool:
@@ -548,19 +346,10 @@ class Dataset(torch.utils.data.Dataset):
             return self.index_select(idx)
 
 
-class Collater(object):
-    def __init__(self, follow_batch, exclude_keys):
-        self.follow_batch = follow_batch
-        self.exclude_keys = exclude_keys
-
-    def collate(self, batch):
-        from utils import TemporalData
-        batch = [TemporalData(**kargs) for kargs in batch]
-        return Batch.from_data_list(batch, self.follow_batch,
-                                    self.exclude_keys)
-
-    def __call__(self, batch):
-        return self.collate(batch)
+def collate(batch):
+    from utils import create_data
+    batch = [create_data(**kargs) for kargs in batch]
+    return combine_batch_data(batch)
 
 
 class DataLoader(torch.utils.data.DataLoader):
@@ -580,17 +369,11 @@ class DataLoader(torch.utils.data.DataLoader):
         **kwargs (optional): Additional arguments of
             :class:`torch.utils.data.DataLoader`.
     """
-    def __init__(self, dataset, batch_size=1, shuffle=False, follow_batch=[],
-                 exclude_keys=[], **kwargs):
+    def __init__(self, dataset, batch_size=1, shuffle=False, **kwargs):
 
         if "collate_fn" in kwargs:
             del kwargs["collate_fn"]
 
-        # Save for PyTorch Lightning...
-        self.follow_batch = follow_batch
-        self.exclude_keys = exclude_keys
-
         super(DataLoader,
               self).__init__(dataset, batch_size, shuffle,
-                             collate_fn=Collater(follow_batch,
-                                                 exclude_keys), **kwargs)
+                             collate_fn=collate, **kwargs)

utils.py
@@ -18,58 +18,20 @@ import torch.nn as nn
 from mypyg.data import Data
 
 
-class TemporalData(Data):
-
-    def __init__(self,
-                 x: Optional[torch.Tensor] = None,
-                 positions: Optional[torch.Tensor] = None,
-                 edge_index: Optional[torch.Tensor] = None,
-                 edge_attrs: Optional[List[torch.Tensor]] = None,
-                 y: Optional[torch.Tensor] = None,
-                 num_nodes: Optional[int] = None,
-                 padding_mask: Optional[torch.Tensor] = None,
-                 bos_mask: Optional[torch.Tensor] = None,
-                 rotate_angles: Optional[torch.Tensor] = None,
-                 lane_vectors: Optional[torch.Tensor] = None,
-                 is_intersections: Optional[torch.Tensor] = None,
-                 turn_directions: Optional[torch.Tensor] = None,
-                 traffic_controls: Optional[torch.Tensor] = None,
-                 lane_actor_index: Optional[torch.Tensor] = None,
-                 lane_actor_vectors: Optional[torch.Tensor] = None,
-                 seq_id: Optional[int] = None,
-                 **kwargs) -> None:
-        if x is None:
-            super(TemporalData, self).__init__()
-            return
-        super(TemporalData, self).__init__(x=x, positions=positions, edge_index=edge_index, y=y, num_nodes=num_nodes,
-                                           padding_mask=padding_mask, bos_mask=bos_mask, rotate_angles=rotate_angles,
-                                           lane_vectors=lane_vectors, is_intersections=is_intersections,
-                                           turn_directions=turn_directions, traffic_controls=traffic_controls,
-                                           lane_actor_index=lane_actor_index, lane_actor_vectors=lane_actor_vectors,
-                                           seq_id=seq_id, **kwargs)
-
-    def __inc__(self, key, value, *args, **kwargs):
-        if key == 'lane_actor_index':
-            return torch.tensor([[self['lane_vectors'].size(0)], [self.num_nodes]])
-        else:
-            return super().__inc__(key, value)
+def create_data(num_nodes: Optional[int] = None, **kwargs) -> Data:
+    kwargs['agent_index'] = torch.IntTensor([kwargs['agent_index']])
+    kwargs = {k: v for k, v in kwargs.items() if torch.is_tensor(v)}
+    data = Data(num_nodes=num_nodes, kwargs=kwargs)
+    return data
 
 
-class DistanceDropEdge(object):
-
-    def __init__(self, max_distance: Optional[float] = None) -> None:
-        self.max_distance = max_distance
-
-    def __call__(self,
-                 edge_index: torch.Tensor,
-                 edge_attr: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        if self.max_distance is None:
-            return edge_index, edge_attr
-        row, col = edge_index
-        mask = torch.norm(edge_attr, p=2, dim=-1) < self.max_distance
-        edge_index = torch.stack([row[mask], col[mask]], dim=0)
-        edge_attr = edge_attr[mask]
-        return edge_index, edge_attr
+def distance_drop_edge(max_distance: float, edge_index: torch.Tensor,
+                       edge_attr: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    row, col = edge_index[0], edge_index[1]
+    mask = torch.norm(edge_attr, p=2, dim=-1) < max_distance
+    edge_index = torch.stack([row[mask], col[mask]], dim=0)
+    edge_attr = edge_attr[mask]
+    return edge_index, edge_attr
 
 
 def init_weights(m: nn.Module) -> None: